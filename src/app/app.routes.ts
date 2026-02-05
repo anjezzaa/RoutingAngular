651f6733cc75a492f8905bda5e21e943367565ce
anjezzaa <anjeza.murati@gmail.com>
2026-02-05 09:38:51 +0100

    redirecting users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,26 @@
 import { Routes  } from "@angular/router"
 
-import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { routes as userRoutes} from './users/users.routes';
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+
 import { TasksComponent } from "./tasks/tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
+
 
 export const routes: Routes = [
     {
         path: '', //<your-domain>/
         component: NoTaskComponent,
+        // redirectTo: '/users/u1',
+        // pathMatch: 'full',
     },
     {
-    path: 'users/:userId', //<your-domain>/users/<uid>
-    component: UserTasksComponent,
-    children: [
-        {
-            path: 'tasks', // <your-domain>/users/<uid>/tasks
-            component: TasksComponent
-        },
-        {
-            path: 'tasks/new',
-            component: NewTaskComponent
-        }
-    ]
+        path: 'users/:userId', //<your-domain>/users/<uid>
+        component: UserTasksComponent,
+        children: userRoutes,
+
     },
     {
         path: '**',
